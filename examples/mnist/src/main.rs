4ec08ce8ce1b5036460d3e0bee32d5b4d94226f7
Jacob Freeman <t00niyf5@duck.com>
2025-11-11 11:17:15 -0500

    chore: Finish up example

--- examples/mnist/src/main.rs
+++ examples/mnist/src/main.rs
@@ -3,22 +3,34 @@ use std::{io::BufReader, fs::File};
 use indicatif::ProgressBar;
 
 fn main() {
-    let epochs = 100000;
+    let epochs = 650;
     
     let mut input_idx = BufReader::new(File::open("src/train-images-idx3-ubyte").unwrap());
     let mut labels_idx = BufReader::new(File::open("src/train-labels-idx1-ubyte").unwrap());
+
+    let mut test_input = BufReader::new(File::open("src/t10k-images-idx3-ubyte").unwrap());
+    let mut test_labels = BufReader::new(File::open("src/t10k-labels-idx1-ubyte").unwrap());
     
     let data: Vec<DataValue> = DataValue::from_data_label_idx(&mut input_idx, &mut labels_idx).unwrap();
+    let test_data: Vec<DataValue> = DataValue::from_data_label_idx(&mut test_input, &mut test_labels).unwrap();
 
     // Network with 784 inputs, 100 hidden, and 10 outputs. Both the hidden layer and the output have sigmoid activation
     let mut network = NeuralNetwork::new(&[100, 10], 784, vec![Activation::Sigmoid, Activation::Sigmoid]).unwrap();
 
     let bar = ProgressBar::new(epochs);
 
-    println!("Learning...");
-    for epoch in 0..epochs {
+    let epoch_size = data.len() / 10;
+
+    println!("Learning... (Epoch size: {epoch_size})");
+    for epoch in 1..=epochs {
         bar.inc(1);
-        network.learn(&data, 0.5);
-        println!("Epoch: {epoch}. Loss: {}", network.loss(&data).unwrap());
+        network.learn_randomly(&data, 0.5, epoch_size);
+        if epoch % 100 == 0 {
+            println!("Epoch: {epoch}. Loss: {}", network.loss(&test_data).unwrap());
+        } else {
+            println!("Epoch: {epoch}.");
+        }
     }
+
+    println!("{network:#?}");
 }
